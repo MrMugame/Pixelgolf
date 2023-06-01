d4139317f6b65f1f6cf0e963991f1b2820d039a6
MrMugame <mr.mugame@gmail.com>
2023-06-01 22:04:05 +0200

    Added HUD

--- src/scenes/levels/components/UIHUD.java
+++ src/scenes/levels/components/UIHUD.java
@@ -3,7 +3,11 @@ package scenes.levels.components;
 import assets.Assets;
 import gui.ConstraintFactory;
 import gui.UIComponent;
+import gui.components.UIImage;
 import gui.components.UIText;
+import gui.constraints.UIEndAlignContstraint;
+import gui.constraints.UIImageAspectConstraint;
+import gui.constraints.UIUnitConstraint;
 
 import java.awt.*;
 
@@ -13,8 +17,12 @@ public class UIHUD extends UIComponent {
 
     @Override
     protected void init() {
-        UIText text = new UIText("HUD", new Color(0,0,0), Assets.DEFAULT_FONT, 20, true);
-        text.setConstraints(ConstraintFactory.unitConstrains(0,0, 10, 10));
-        add(text);
+
+        UIImage image = new UIImage("ui/hud_star_0.png");
+        image.getConstraints().addX(new UIEndAlignContstraint());
+        image.getConstraints().addY(new UIUnitConstraint(0));
+        image.getConstraints().addWidth(new UIUnitConstraint(10));
+        image.getConstraints().addHeight(new UIImageAspectConstraint());
+        add(image);
     }
 }
